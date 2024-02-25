7d951ad1dca901ff13bb810e0065e9da4477e729
Julian L <julian.lawrance@gmail.com>
2024-02-25 09:56:25 +0000

    Add short description to each flyer on history page

--- src/components/FlyerHistory.vue
+++ src/components/FlyerHistory.vue
@@ -1,16 +1,52 @@
 <script setup lang="ts">
+import { ref } from "vue"
+
+import type { Flyer } from "../data/Flyer"
+
 import { useFlyerHistoryStore } from "../stores/flyerHistory"
+import { differenceInMinutes } from "date-fns"
 
 const flyerHistoryStore = useFlyerHistoryStore()
+
+const selectedFlyer = ref("")
+
+const setSelectedFlyer = (f: Flyer) => {
+    if (selectedFlyer.value !== f.id) {
+        selectedFlyer.value = f.id
+    }
+    else {
+        selectedFlyer.value = ""
+    }
+}
+
+const isSelected = (f: Flyer) => selectedFlyer.value === f.id
+
+const getDuration = (f: Flyer) => {
+    if (!f.startTime || !f.finishTime) {
+        return "???"
+    }
+
+    return differenceInMinutes(new Date(f.finishTime), new Date(f.startTime))
+}
+
+const getWinnerName = (f: Flyer) => flyerHistoryStore.getWinner(f)?.name || "???"
 </script>
 
 <template>
-    <div v-for="f in flyerHistoryStore.pastFlyers"
-        class="flex justify-content-between cursor-pointer">
-        <div>
-            {{ f.settings.name }}
+    <div v-for="f, i in flyerHistoryStore.pastFlyers" class="cursor-pointer" @click="() => setSelectedFlyer(f)">
+        <div class="flex justify-content-between mt-1 pt-1 mb-1" :class="[i > 0 && 'border-gray-200 border-top-1']">
+            <div :class="[isSelected(f) && 'font-bold']">
+                {{ f.settings.name }}
+            </div>
+
+            <div :class="[isSelected(f) && 'font-bold']">
+                {{ f.startTime }}
+            </div>
         </div>
 
-        <div>{{ f.startTime }}</div>
+        <div v-if="isSelected(f)" class="font-italic">
+            {{ f.settings.format }} between {{ f.players.length }} players, races to {{ f.settings.raceTo }}.&nbsp;
+            Took {{ getDuration(f) }} minute(s), won by {{ getWinnerName(f) }}.
+        </div>
     </div>
 </template>
--- src/stores/flyerHistory.ts
+++ src/stores/flyerHistory.ts
@@ -2,6 +2,7 @@ import { useStorage } from "@vueuse/core"
 import { defineStore } from "pinia"
 
 import type { Flyer } from "../data/Flyer"
+import type { Result } from "../data/Result"
 
 export const useFlyerHistoryStore = defineStore("flyerHistory", () => {
     const pastFlyers = useStorage("pastFlyers", <Flyer[]>[])
@@ -10,9 +11,24 @@ export const useFlyerHistoryStore = defineStore("flyerHistory", () => {
         pastFlyers.value = [...pastFlyers.value, flyer]
     }
 
+    const getWinner = (flyer: Flyer) => {
+        const finalRound = flyer.rounds[flyer.rounds.length - 1]
+        if (finalRound) {
+            const final = finalRound.fixtures[finalRound.fixtures.length - 1]
+            const id = getResultWinner(final).playerId
+            return flyer.players.find(p => p.id === id) || null
+        }
+
+        return null
+    }
+
+    const getResultWinner = (f: Result) => f.scores.reduce((s, t) => s.score > t.score ? s : t)
+
     return {
         pastFlyers,
 
         add,
+
+        getWinner,
     }
 })
