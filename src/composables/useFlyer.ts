24c97c108fa05f200471c7c00ebceb1126bd678b
Julian L <julian.lawrance@gmail.com>
2024-02-27 09:45:29 +0000

    Create useFlyer() composable for tracking flyer progress

---
+++ src/composables/useFlyer.ts
@@ -0,0 +1,59 @@
+import { computed, ref, watch } from "vue"
+import { useIntervalFn } from "@vueuse/core"
+import { differenceInSeconds } from "date-fns"
+
+import type { Flyer } from "../data/Flyer"
+
+export const useFlyer = (f: Flyer) => {
+    const flyer = ref(f)
+
+    const hasStarted = computed(() => !!flyer.value.startTime)
+    const hasFinished = computed(() => !!flyer.value.finishTime)
+    const isInProgress = computed(() => hasStarted.value && !hasFinished.value)
+
+    const durationSeconds = computed(() => {
+        if (!hasStarted.value || !hasFinished.value) {
+            return null
+        }
+
+        return Math.floor((flyer.value.finishTime! - flyer.value.startTime!) / 1000)
+    })
+
+    const computeDifference = () => differenceInSeconds(Date.now(), flyer.value.startTime || Date.now())
+
+    const elapsedSeconds = ref(computeDifference())
+
+    const updateClock = () => {
+        const newValue = computeDifference()
+        console.debug("FlyerClock " + flyer.value.settings.name + ": " + flyer.value.startTime + " + " + newValue)
+        elapsedSeconds.value = newValue
+    }
+
+    watch(flyer, () => {
+        updateClock()
+
+        if (flyer.value.startTime) {
+            resumeClock()
+        }
+    })
+
+    const clock = useIntervalFn(updateClock, 1000, {
+        immediate: false,
+    })
+
+    const pauseClock = clock.pause
+    const resumeClock = clock.resume
+
+    return {
+        flyer,
+
+        elapsedSeconds,
+        hasStarted,
+        hasFinished,
+        isInProgress,
+        durationSeconds,
+
+        pauseClock,
+        resumeClock,
+    }
+}
