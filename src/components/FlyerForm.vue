c694d424945a81d990968cf996e80f0ac0cb8612
Julian L <julian.lawrance@gmail.com>
2024-03-10 14:51:04 +0000

    Adjust TODO item

--- src/components/FlyerForm.vue
+++ src/components/FlyerForm.vue
@@ -67,7 +67,7 @@ onUpdated(() => {
 <template>
     <TabMenu class="mb-2" :model="items" />
 
-    <!-- LOW: only make the form content overflow with a scroll bar, not the tab menu also -->
+    <!-- MEDIUM: only make the form content overflow with a scroll bar, not the tab menu also -->
     <div id="form-content">
         <div v-if="section === Section.Players">
             <div class="p-fluid mb-2 md:hidden">
