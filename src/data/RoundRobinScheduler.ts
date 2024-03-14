af263056de33f3ca6e84dda03c82141437a9f500
Julian L <julian.lawrance@gmail.com>
2024-03-14 13:00:25 +0000

    Refactor round-robin round creation into its own method

--- src/data/RoundRobinScheduler.ts
+++ src/data/RoundRobinScheduler.ts
@@ -33,63 +33,16 @@ export class RoundRobinScheduler implements IScheduler {
 
         let attempts = 0
 
-        const oddPlayerCount = players.length % 2 !== 0
-        const numRounds = oddPlayerCount ? players.length : players.length - 1
+        const numRounds = players.length % 2 !== 0 ? players.length : players.length - 1
 
         this.generatedRounds = <Round[]>[]
 
         // use this to omit a random player each round, if we have an odd number of players
         const omissionIndexes = this.shuffle(players.map((_, i) => i))
 
-        // generate rounds of fixtures. For each round:
-        // 1. create an overall pool of all players. If the number of players is odd, omit one player who has NOT yet been omitted from a round from this round
-        // 2. select a random player ID from the overall pool as player A. E.g. 1 from [1, 2, 3, 4]
-        // 3. extract, from ALL the existing fixtures, the IDs of players against whom they already have a fixture. E.g. [2]
-        // 4. select a random player ID from the pool, EXCLUDING that extracted set of IDs and player A's ID, as player B. E.g. 4 from [3, 4]
-        // 5. add a fixture for player A vs player B to this round. E.g. 1v4
-        // 6. remove those IDs from the overall pool. E.g. [2, 3]
-        // 7. repeat steps 1-5 until the overall pool has fewer than 2 players
-        // 8. add the round to the list of rounds
         let r = 0
         while (r < numRounds && attempts < this.generationAttempts) {
-            const round = <Round>{
-                index: r + 1,
-                name: "Round " + (r + 1),
-                isGenerated: true,
-                fixtures: [],
-            }
-
-            console.debug(round.name)
-
-            let retry = false
-
-            let overallPool = [...players]
-
-            if (oddPlayerCount) {
-                // omit a random player that hasn't been omitted yet
-                console.debug("Omitting: " + overallPool[omissionIndexes[r]].name)
-                overallPool.splice(omissionIndexes[r], 1)
-            }
-
-            while (overallPool.length > 1) {
-                const playerA = this.getRandom(overallPool)
-
-                const existingOpponents = this.generatedRounds.flatMap(r => this.getExistingOpponents(r, playerA))
-
-                const possibleOpponents = overallPool.filter(p => !existingOpponents.includes(p.id) && playerA.id !== p.id)
-                if (possibleOpponents.length <= 0) {
-                    // just keep retrying for now... address this deterministically later!
-                    console.debug("Retrying round...")
-                    retry = true
-                    break
-                }
-
-                const playerB = this.getRandom(possibleOpponents)
-
-                this.addFixture(round, [playerA, playerB])
-
-                overallPool = overallPool.filter(p => ![playerA.id, playerB.id].includes(p.id))
-            }
+            const [retry, round] = this.createRound(r + 1, players, omissionIndexes[r])
 
             if (retry) {
                 attempts++
@@ -104,9 +57,62 @@ export class RoundRobinScheduler implements IScheduler {
             throw `Failed to generate rounds after ${this.generationAttempts} attempt(s)!`
         }
 
-        // MEDIUM: generate more stages if stageCount > 1
+        if (this.stageCount > 1) {
+            // TODO: collect the existing fixtures up into one list, and
+            // distribute them randomly into another set of rounds
+        }
 
         return this.generatedRounds
+    }
+
+    private createRound(index: number, players: Player[], omissionIndex: number): [boolean, Round] {
+        // 1. create an overall pool of all players. If the number of players is odd, omit one player who has NOT yet been omitted from a round from this round
+        // 2. select a random player ID from the overall pool as player A. E.g. 1 from [1, 2, 3, 4]
+        // 3. extract, from ALL the existing fixtures, the IDs of players against whom they already have a fixture. E.g. [2]
+        // 4. select a random player ID from the pool, EXCLUDING that extracted set of IDs and player A's ID, as player B. E.g. 4 from [3, 4]
+        // 5. add a fixture for player A vs player B to this round. E.g. 1v4
+        // 6. remove those IDs from the overall pool. E.g. [2, 3]
+        // 7. repeat steps 1-5 until the overall pool has fewer than 2 players
+        const round = <Round>{
+            index,
+            name: "Round " + index,
+            isGenerated: true,
+            fixtures: [],
+        }
+
+        console.debug(round.name)
+
+        let retry = false
+
+        let overallPool = [...players]
+
+        if (players.length % 2 !== 0) {
+            // omit a random player that hasn't been omitted yet
+            console.debug("Omitting: " + overallPool[omissionIndex].name)
+            overallPool.splice(omissionIndex, 1)
+        }
+
+        while (overallPool.length > 1) {
+            const playerA = this.getRandom(overallPool)
+
+            const existingOpponents = this.generatedRounds!.flatMap(r => this.getExistingOpponents(r, playerA))
+
+            const possibleOpponents = overallPool.filter(p => !existingOpponents.includes(p.id) && playerA.id !== p.id)
+            if (possibleOpponents.length <= 0) {
+                // just keep retrying for now... address this deterministically later!
+                console.debug("Retrying round...")
+                retry = true
+                break
+            }
+
+            const playerB = this.getRandom(possibleOpponents)
+
+            this.addFixture(round, [playerA, playerB])
+
+            overallPool = overallPool.filter(p => ![playerA.id, playerB.id].includes(p.id))
+        }
+
+        return [retry, round]
     }
 
     private addFixture(round: Round, players: Player[]) {
