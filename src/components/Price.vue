35b24dde8a04ca646ee42ad87f3f424a63bfc7c5
Julian L <julian.lawrance@gmail.com>
2024-03-22 23:13:30 +0000

    Create component for showing a price

---
+++ src/components/Price.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, ref, watch } from "vue"
+import { useI18n } from "vue-i18n"
+
+const props = defineProps<{
+    amount: number
+    large?: boolean
+}>()
+
+const { n } = useI18n()
+
+const amount = ref(props.amount)
+
+watch(props, () => {
+    amount.value = props.amount
+})
+
+const priceText = computed(() => n(amount.value, "currency"))
+</script>
+
+<template>
+    <p class="m-0 text-center font-bold text-xl" :class="[props.large && 'text-3xl']">
+        {{ priceText }}
+    </p>
+</template>
