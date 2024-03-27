908b0063dd1179de2298e03f32eeb688847950f5
Julian L <julian.lawrance@gmail.com>
2024-03-27 23:36:15 +0000

    Add prop to Clock component for turning the display red if the elapsed time exceeds a given amount

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -3,6 +3,7 @@ import { computed, ref, watch } from "vue"
 
 const props = defineProps<{
     elapsedSeconds: number
+    warnAfterSeconds?: number
     large?: boolean
 }>()
 
@@ -12,9 +13,15 @@ watch(props, () => {
     elapsedSeconds.value = props.elapsedSeconds
 })
 
+// MEDIUM: encapsulate this inside a Clock object, which should be exported
+// by a useClock() hook, which should take an object with startTime and
+// finishTime properties
+const shouldWarn = computed(() => !!props.warnAfterSeconds && elapsedSeconds.value > props.warnAfterSeconds)
+
 const pad = (n: number) => n.toString().padStart(2, "0")
 
 const clockText = computed(() => {
+    // MEDIUM: use vue-i18n for this time formatting
     const hours = Math.floor(elapsedSeconds.value / 3600)
     const r = elapsedSeconds.value % 3600
     const minutes = Math.floor(r / 60)
@@ -25,7 +32,12 @@ const clockText = computed(() => {
 </script>
 
 <template>
-    <p class="m-0 text-center font-bold text-xl" :class="[props.large && 'text-3xl']">
+    <p
+        class="m-0 text-center font-bold text-xl"
+        :class="[
+            props.large && 'text-3xl',
+            shouldWarn && 'text-red-500',
+        ]">
         {{ clockText }}
     </p>
 </template>
