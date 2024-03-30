043d8d2312b1862271f3ab439f6ab0ba493d9ffe
Julian L <julian.lawrance@gmail.com>
2024-03-30 22:45:59 +0000

    Create PhaseSettings interface for encapsulating an established specification for a phase

---
+++ src/data/PhaseSettings.ts
@@ -0,0 +1,68 @@
+import type { Phase } from "./Phase"
+import type { PlayOff } from "./PlayOff"
+
+export interface PhaseSettings {
+    matchLengthModel: MatchLengthModel
+    raceTo: number
+    winsRequired: number
+    ruleSet: RuleSet
+    format: Format
+    stageCount: number
+    randomlyDrawAllRounds: boolean
+    requireCompletedRounds: boolean
+    allowDraws: boolean
+    allowEarlyFinish: boolean
+    tieBreaker: TieBreaker
+    entryFeeRequired: boolean
+    entryFee: number
+    moneySplit: MoneySplit
+    name: string
+}
+
+export enum MatchLengthModel {
+    Fixed,
+    Variable,
+}
+
+export enum Format {
+    Knockout,
+    RoundRobin,
+    WinnerStaysOn,
+    // LOW: implement "vs The Ghost" format for one player
+}
+
+export enum RuleSet {
+    Blackball,
+    International,
+}
+
+export enum TieBreaker {
+    HeadToHead,
+    Runouts,
+    PlayOff,
+}
+
+export enum MoneySplit {
+    WinnerTakesAll,
+    SeventyThirty,
+    SixtyTwentyFiveFifteen,
+    SemiFinalists,
+}
+
+export const createPlayOffSettings = (phase: Phase, playOff: PlayOff): PhaseSettings => ({
+    allowDraws: false,
+    allowEarlyFinish: false,
+    entryFee: 0,
+    entryFeeRequired: false,
+    format: Format.Knockout,
+    matchLengthModel: MatchLengthModel.Fixed,
+    moneySplit: MoneySplit.WinnerTakesAll,
+    name: playOff.name,
+    raceTo: 1,
+    randomlyDrawAllRounds: false,
+    requireCompletedRounds: true,
+    ruleSet: phase.settings.ruleSet,
+    stageCount: 1,
+    tieBreaker: phase.settings.tieBreaker,
+    winsRequired: 0,
+})
