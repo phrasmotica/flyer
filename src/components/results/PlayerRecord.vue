7a2b3c7dfdc5e548c17bd04ebaf5c03699632068
Julian Lawrance <julian.lawrance@gmail.com>
2024-04-20 01:48:15 +0100

    Show a message in expanded results table row if a player has not yet played any fixtures

--- src/components/results/PlayerRecord.vue
+++ src/components/results/PlayerRecord.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { computed } from "vue"
+import { useI18n } from "vue-i18n"
+
+import MatchText from "./MatchText.vue"
 
 import { useFixtureList } from "@/composables/useFixtureList"
 import { useFlyer } from "@/composables/useFlyer"
 
 import { useFlyerStore } from "@/stores/flyer"
-import MatchText from "./MatchText.vue"
+
+const { t } = useI18n()
 
 const props = defineProps<{
     playerId: string
@@ -21,15 +25,20 @@ const {
     getFixtures,
 } = useFixtureList(mainPhase.value)
 
-const fixtures = computed(() => getFixtures(props.playerId))
+const fixtures = computed(() => getFixtures(props.playerId).filter(f => f.finishTime))
 </script>
 
 <template>
-    <div>
+    <div v-if="fixtures.length > 0">
         <ul class="m-0">
             <li v-for="f in fixtures" class="text-sm">
                 <MatchText :playerId="props.playerId" :fixture="f" />
             </li>
         </ul>
+    </div>
+    <div v-else>
+        <span class="text-sm">
+            {{ t('playerRecord.noFixturesPlayed') }}
+        </span>
     </div>
 </template>
