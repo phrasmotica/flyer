5401d9fcdb4bc1a978a584efcc39d7d2de921c59
Julian Lawrance <julian.lawrance@gmail.com>
2024-04-23 21:59:53 +0100

    Create component for showing a preset's info

---
+++ src/components/theming/PresetInfo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { Preset } from "@/stores/presets"
+
+const props = defineProps<{
+    preset: Preset
+}>()
+
+const emit = defineEmits<{
+    load: []
+    delete: []
+}>()
+</script>
+
+<template>
+    <div class="flex align-items-center justify-content-between mt-2">
+        <span class="text-sm">
+            {{ props.preset.name }}
+        </span>
+
+        <div class="flex gap-2 ml-2">
+            <Button
+                icon="pi pi-upload"
+                severity="warning"
+                @click="() => emit('load')" />
+
+            <Button
+                icon="pi pi-trash"
+                severity="danger"
+                @click="() => emit('delete')" />
+        </div>
+    </div>
+</template>
--- src/components/theming/PresetsMenu.vue
+++ src/components/theming/PresetsMenu.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import PresetInfo from "./PresetInfo.vue"
+
 import { usePresetsStore } from "@/stores/presets"
 import { useSettingsStore } from "@/stores/settings"
 
@@ -29,21 +31,10 @@ const saveNewPreset = () => {
         <div v-else>
             <div v-for="p of presetsStore.presets"
                 class="flex align-items-center justify-content-between mt-2">
-                <span class="text-sm">
-                    {{ p.name }}
-                </span>
-
-                <div class="flex gap-2 ml-2">
-                    <Button
-                        icon="pi pi-upload"
-                        severity="warning"
-                        @click="() => settingsStore.importSettings(p.settings)" />
-
-                    <Button
-                        icon="pi pi-trash"
-                        severity="danger"
-                        @click="() => presetsStore.deletePreset(p.id)" />
-                </div>
+                <PresetInfo
+                    :preset="p"
+                    @load="() => settingsStore.importSettings(p.settings)"
+                    @delete="() => presetsStore.deletePreset(p.id)" />
             </div>
         </div>
     </div>
