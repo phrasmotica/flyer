944fa809e83036612998c701e9f7d43756a8bba3
Julian Lawrance <julian.lawrance@gmail.com>
2024-04-23 13:00:02 +0100

    Fix width of CurrencyStepper's input element

--- src/components/setup/CurrencyStepper.vue
+++ src/components/setup/CurrencyStepper.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { v4 as uuidv4 } from "uuid"
 import { computed, onUpdated } from "vue"
 import { useI18n } from "vue-i18n"
-import { v4 as uuidv4 } from "uuid"
 
+import { useScreenSizes } from "@/composables/useScreenSizes"
 import { useTweaks } from "@/composables/useTweaks"
 
 const value = defineModel<number>()
@@ -17,6 +18,10 @@ const props = defineProps<{
 
 const { locale, numberFormats } = useI18n()
 
+const {
+    isSmallScreen,
+} = useScreenSizes()
+
 const currency = computed(() => {
     const currentLocale = numberFormats.value[locale.value]
     if (currentLocale) {
@@ -30,32 +35,47 @@ const { blurNumberInputs } = useTweaks()
 
 const id = "currency-stepper-" + uuidv4()
 
+const inputClass = computed(() => {
+    let c = "text-center font-bold"
+
+    if (isSmallScreen.value) {
+        c += " text-sm"
+    }
+
+    return c
+})
+
 onUpdated(() => {
     blurNumberInputs(id)
 })
 </script>
 
 <template>
-    <!-- BUG: too wide when used inside <AddTableModal> on 320px mobile. The
-        <input> element itself can have its width set lower to resolve things,
-        but it and the stepper buttons are still encased in a <span>, god knows
-        why... how can we get the <input> element to only be as wide as is
-        allowed by the modal? Try putting this element inside a
-        <div class="flex justify-content-center"> -->
-    <InputNumber
-        :id="id"
-        v-model="value"
-        showButtons buttonLayout="horizontal"
-        :min="props.min || 0" :max="props.max || 100" :step="props.step || 0.5"
-        mode="currency" :currency="currency" :locale="locale"
-        :suffix="props.suffix"
-        :inputId="props.inputId"
-        inputClass="text-center font-bold">
-        <template #incrementbuttonicon>
-            <span class="pi pi-plus" />
-        </template>
-        <template #decrementbuttonicon>
-            <span class="pi pi-minus" />
-        </template>
-    </InputNumber>
+    <div class="flex justify-content-center">
+        <!-- we provide a nominal narrow width to the inputStyle, so that
+        the flex-grow: 1 in the style block will always activate -->
+        <InputNumber
+            :id="id"
+            v-model="value"
+            showButtons buttonLayout="horizontal"
+            :min="props.min || 0" :max="props.max || 100" :step="props.step || 0.5"
+            mode="currency" :currency="currency" :locale="locale"
+            :suffix="props.suffix"
+            :inputId="props.inputId"
+            :inputClass="inputClass"
+            :inputStyle="{ width: '100px' }">
+            <template #incrementbuttonicon>
+                <span class="pi pi-plus" />
+            </template>
+            <template #decrementbuttonicon>
+                <span class="pi pi-minus" />
+            </template>
+        </InputNumber>
+    </div>
 </template>
+
+<style>
+.p-inputnumber {
+    flex-grow: 1;
+}
+</style>
