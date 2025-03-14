1cf3ea271dc2d05bb4c3d10c2054f50ab12b87d2
Julian L <julian.lawrance@gmail.com>
2025-03-14 20:17:55 +0000

    Add input switch to FixtureModal, allowing the fixture to be toggled

---
+++ src/components/play/FixtureExcludeToggle.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import InputSwitch from "primevue/inputswitch"
+
+import type { Fixture } from "@/data/Fixture"
+
+const emit = defineEmits<{
+    setExcluded: [excluded: boolean]
+}>()
+
+const props = defineProps<{
+    fixture: Fixture
+}>()
+</script>
+
+<template>
+    <div class="flex justify-content-between">
+        <span>Exclude?</span>
+
+        <InputSwitch
+            :modelValue="fixture.isExcluded"
+            @update:modelValue="c => emit('setExcluded', c)" />
+    </div>
+</template>
